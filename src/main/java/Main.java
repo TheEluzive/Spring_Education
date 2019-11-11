548dc00bca3638d022164dc95035538266aa6317
Evgeny Tkach <1eluzive1@gmail.com>
2019-11-12 00:50:30 +0400

    lesson6 p.3

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,7 +1,9 @@
+import java.io.IOException;
+
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 public class Main {
 	@SuppressWarnings("unused")
-	public static void main(String[] args) {
+	public static void main(String[] args) throws IOException {
 		System.out.println("Hello World!");
 		ClassPathXmlApplicationContext classPathXMLContext = new ClassPathXmlApplicationContext("applicationContext.xml");
 	
@@ -10,6 +12,8 @@ public class Main {
 		MusicPlayer musicPlayer = classPathXMLContext.getBean("musicPlayer", MusicPlayer.class);
 
         musicPlayer.playMusic();
+        System.out.println(musicPlayer.getVolume());
 		classPathXMLContext.close();
+		System.in.read();
 	}
 }
--- src/main/java/MusicPlayer.java
+++ src/main/java/MusicPlayer.java
@@ -1,22 +1,37 @@
-import lombok.Setter;
-import lombok.NoArgsConstructor;
-//@Setter 
+import lombok.Getter;
+
+
 public class MusicPlayer {
 	private IMusic music;
+	private int volume;
+	private String name;
 	
 	
-	
-	/*public void setMusic(IMusic music) {
-		this.music = music;
-	}*/
 	public MusicPlayer(IMusic music) {
 		this.music = music;
 	}
 	public MusicPlayer() {}
 	
-	public void playMusic(){
-	System.out.println(music.getSong());
+	public void setVolume(int volume) {
+		this.volume = volume;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public void setMusic(IMusic music) {
+		this.music = music;
+	}
 	
+	public int getVolume() {
+		return volume;
+	}
+	public String getName() {
+		return name;
+	}
+	
+	public void playMusic(){
+	System.out.println("Playing: " + music.getSong());	
 	}
 
 
