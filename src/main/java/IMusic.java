d9878120740a476d85476d80162c9efb8990c125
Evgeny Tkach <1eluzive1@gmail.com>
2019-11-12 00:15:26 +0400

    lesson6 p.2

--- src/main/java/IMusic.java
+++ src/main/java/IMusic.java
@@ -1,6 +1,6 @@
 
 public interface IMusic {
-	;
+	
 	String getSong();
 
 }
--- src/main/java/MusicPlayer.java
+++ src/main/java/MusicPlayer.java
@@ -1,14 +1,21 @@
-
+import lombok.Setter;
+import lombok.NoArgsConstructor;
+//@Setter 
 public class MusicPlayer {
 	private IMusic music;
 	
+	
+	
+	/*public void setMusic(IMusic music) {
+		this.music = music;
+	}*/
 	public MusicPlayer(IMusic music) {
 		this.music = music;
 	}
+	public MusicPlayer() {}
 	
 	public void playMusic(){
 	System.out.println(music.getSong());
-	
 	
 	}
 
